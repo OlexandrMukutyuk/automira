5bd2fcdb7f858580745582dfb92bb0764c7ecb40
Dima Kacevich <d.katsevich.4@gmail.com>
2023-12-15 16:17:05 +0200

    Remove order editing before proving

--- app/Http/Controllers/Api/In/ProveOrderController.php
+++ app/Http/Controllers/Api/In/ProveOrderController.php
@@ -15,37 +15,7 @@ class ProveOrderController
 
 
         try {
-
-            $orderData = post_automira('/getInOrder', [
-                'ID' => $data['id'],
-            ])[0];
-
-
-            $orderProducts = collect($orderData['products'])
-                ->filter(fn(array $product) => $product['count'] > 0)
-                ->map(fn(array $product) => [
-                    'article' => $product['productId'],
-                    'count' => $product['count'],
-                    'shelf' => $product['shelfId'],
-                ])
-                ->values()
-                ->toArray();
-
-
-            $editData = [
-                'id' => $data['id'],
-                'agent' => $orderData['agentID'],
-                'storage' => $orderData['storageId'],
-                'responsible' => $orderData['responsibleId'],
-                'productArray' => $orderProducts,
-                'date' => now()->toDateTimeLocalString(),
-            ];
-
-
-            post_automira('/editInOrder', $editData)[0];
-
-            post_automira('/provInOrder', $data)[0];
-            
+            post_automira('/provInOrder', $data);
         } catch (AutomiraException $e) {
             return response()->noContent(400);
         }
